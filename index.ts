a1da691c40024293c514a03ede4f606aefbd4973
Tanzeela Arshad <aimaanmol37@gmail.com>
2024-06-04 17:09:53 +0500

    it's shabang

---
+++ index.ts
@@ -0,0 +1,23 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+// Declare a constant answer amd assign it to the inquirer.prompt function
+
+const answers: {
+    sentence: string
+} = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: "Enter your sentence to count the words: "
+    }
+])
+
+const words = answers.sentence.trim().split(" ")
+
+// Print the array of words to the comsole 
+console.log(words);
+
+//Print the word count of the sentence to the comsole 
+console.log(`Your sentence word count is ${words.length}`) ;
